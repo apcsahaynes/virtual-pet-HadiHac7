ed59bd91004d5e4ba6eda18fb5fcea49767e7c3d
HadiHac7 <hadi.hachem@jpschools.org>
2026-03-09 11:26:14 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,27 @@
 public class Game {
+    private String name;
+    private int happinessIncrease;
+    private int weightLoss;
+
+    public Game(String name, int happinessIncrease, int weightLoss) {
+        this.name = name;
+        this.happinessIncrease = happinessIncrease;
+        this.weightLoss = weightLoss;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHappinessIncrease() {
+        return happinessIncrease;
+    }
     
+    public int getWeightLoss() {
+        return weightLoss;
+    }
+
+    public boolean isWinner() {
+        return Math.random() < 0.5;
+    }
 }
